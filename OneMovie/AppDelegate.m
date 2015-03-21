eb1b365c8de77bd3ff24979e513311bb3d268258
541804668 <yuliyang@yumatoMacBook-Air.local>
2015-03-21 14:25:37 +0800

    合并

--- OneMovie/AppDelegate.m
+++ OneMovie/AppDelegate.m
@@ -9,6 +9,7 @@
 #import "AppDelegate.h"
 #import "MainViewController.h"
 
+
 @interface AppDelegate ()
 
 @end
---
+++ OneMovie/DropViewController.h
@@ -0,0 +1,13 @@
+//
+//  DropViewController.h
+//  OneMovie
+//
+//  Created by 俞 丽央 on 15-3-21.
+//  Copyright (c) 2015年 李超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DropViewController : UIViewController
+
+@end
