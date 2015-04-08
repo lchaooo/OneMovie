b0ebae13f4157fc9a13951ae753928b092d712b5
lchaooo <lichao@lichaodeMacBook-Air.local>
2015-04-08 19:10:00 +0800

    wrong

--- OneMovie/MainViewController.m
+++ OneMovie/MainViewController.m
@@ -32,6 +32,8 @@
 @property (strong,nonatomic) YTKKeyValueStore *store;//数据储存
 @property (strong,nonatomic) NSString *tableName;//fmdb tablename
 @property (strong,nonatomic) WebModel *model;
+@property (strong,nonatomic) MovieView *movieView;
+@property (strong,nonatomic) BookView *bookView;
 
 
 @end
@@ -62,22 +64,25 @@
     
     
     //iphone6 frame
-    _contentView = [[ContentView alloc] initWithFrame:CGRectMake(62.5, 150, 250, 354)];
+    //_contentView = [[ContentView alloc] initWithFrame:CGRectMake(62.5, 150, 250, 354)];
     //iphone 5s frame
     //_contentView = [[ContentView alloc] initWithFrame:CGRectMake(55, 100, 210, 300)];
     [self.view addSubview:_contentView];
     
-    _contentView.posterImage.image = nil;
-    _contentView.posterImage.layer.cornerRadius = 10;
-    _contentView.posterImage.clipsToBounds=YES;
+//    _contentView.posterImage.image = nil;
+//    _contentView.posterImage.layer.cornerRadius = 10;
+//    _contentView.posterImage.clipsToBounds=YES;
+//    
+//    UITapGestureRecognizer *tapGR = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(imageClicked)];
+//    [_contentView.posterImage addGestureRecognizer:tapGR];
+//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(showMovieDetails) name:@"MovieDictionary has been downloaded" object:nil];
+//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(disappearAndOpenSafari) name:@"Please Fade Out" object:nil];
+//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(showNoticeOfFailure) name:@"Net is not working" object:nil];
+//    [self showMovieDetails];
     
-    UITapGestureRecognizer *tapGR = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(imageClicked)];
-    [_contentView.posterImage addGestureRecognizer:tapGR];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(showMovieDetails) name:@"MovieDictionary has been downloaded" object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(disappearAndOpenSafari) name:@"Please Fade Out" object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(showNoticeOfFailure) name:@"Net is not working" object:nil];
-    [self showMovieDetails];
-    
+    _movieView = [[MovieView alloc] initWithFrame:CGRectMake(0, [[UIScreen mainScreen] bounds].size.height*0.3, [[UIScreen mainScreen] bounds].size.width, [[UIScreen mainScreen] bounds].size.height*0.7)];
+    _movieView.userInteractionEnabled = YES;
+    [self.view addSubview:_movieView];
     
     SwitchView *sv = [[SwitchView alloc]initWithFrame:CGRectMake(62.5, 60, 250, 50)];
     sv.backgroundColor = [UIColor clearColor];
@@ -94,108 +99,115 @@
 
 #pragma CustomMethods
 //随即选择电影并发出网络请求
-- (void)getMovieIDAndSendRequest{
-    NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"MovieID" ofType:@"plist"];
-    NSMutableArray *movieIDArray = [[NSMutableArray alloc] initWithContentsOfFile:plistPath];
-    NSString *ID = [movieIDArray objectAtIndex:arc4random()%249];
-    [_model getMovieDictionaryByMovieID:ID];
-}
+//- (void)getMovieIDAndSendRequest{
+//    NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"MovieID" ofType:@"plist"];
+//    NSMutableArray *movieIDArray = [[NSMutableArray alloc] initWithContentsOfFile:plistPath];
+//    NSString *ID = [movieIDArray objectAtIndex:arc4random()%249];
+//    [_model getMovieDictionaryByMovieID:ID];
+//}
 
 //页面显示
-- (void)showMovieDetails{
-    
-    NSDictionary *dic = [_store getObjectById:@"movie" fromTable:_tableName];
-    
-    if (dic) {
-    
-        //nameLabel
-        _nameLabel.text = dic[@"title"];
-    
-        //ratingLabel
-        NSString *rating = [NSString stringWithFormat:@"评分：%@",dic[@"rating"][@"average"]];
-        if ([rating length]>6) {
-            rating = [rating substringToIndex:6];
-        }
-        _ratingLabel.text = rating;
-        
-        //typeLabel
-        NSString *type = @"类型：";
-        for (int i=0; i<[dic[@"genres"] count]; i++) {
-            type = [type stringByAppendingString:[NSString stringWithFormat:@"%@/",[dic[@"genres"] objectAtIndex:i]]];
-        }
-        NSString *realType = [type substringToIndex:[type length]-1];
-        _typeLabel.text = realType;
-    
-        [_nameLabel shine];
-        [_ratingLabel shine];
-        [_typeLabel shine];
-    
-        
-        //poster
-        [_contentView.posterImage startLoaderWithTintColor:[UIColor blackColor]];
-        __weak typeof(self)weakSelf = self;
-        [_contentView.posterImage sd_setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@",dic[@"images"][@"large"]]] placeholderImage:[UIImage imageNamed:@"透明.png"] options:SDWebImageCacheMemoryOnly |       SDWebImageRefreshCached progress:^(NSInteger receivedSize,NSInteger expectedSize){
-            [weakSelf.contentView.posterImage updateImageDownloadProgress:(CGFloat)receivedSize/expectedSize];
-        }completed:^(UIImage *image,NSError *error,SDImageCacheType cacheType,NSURL *imageURL){
-            [weakSelf.contentView.posterImage reveal];
-            weakSelf.backgroundImage.image = image;
-            weakSelf.contentView.posterImage.userInteractionEnabled = YES;
-        }];
-    }else{
-        [self getMovieIDAndSendRequest];
-    }
-}
+//- (void)showMovieDetails{
+//    
+//    NSDictionary *dic = [_store getObjectById:@"movie" fromTable:_tableName];
+//    
+//    if (dic) {
+//    
+//        //nameLabel
+//        _nameLabel.text = dic[@"title"];
+//    
+//        //ratingLabel
+//        NSString *rating = [NSString stringWithFormat:@"评分：%@",dic[@"rating"][@"average"]];
+//        if ([rating length]>6) {
+//            rating = [rating substringToIndex:6];
+//        }
+//        _ratingLabel.text = rating;
+//        
+//        //typeLabel
+//        NSString *type = @"类型：";
+//        for (int i=0; i<[dic[@"genres"] count]; i++) {
+//            type = [type stringByAppendingString:[NSString stringWithFormat:@"%@/",[dic[@"genres"] objectAtIndex:i]]];
+//        }
+//        NSString *realType = [type substringToIndex:[type length]-1];
+//        _typeLabel.text = realType;
+//    
+//        [_nameLabel shine];
+//        [_ratingLabel shine];
+//        [_typeLabel shine];
+//    
+//        
+//        //poster
+//        [_contentView.posterImage startLoaderWithTintColor:[UIColor blackColor]];
+//        __weak typeof(self)weakSelf = self;
+//        [_contentView.posterImage sd_setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@",dic[@"images"][@"large"]]] placeholderImage:[UIImage imageNamed:@"透明.png"] options:SDWebImageCacheMemoryOnly |       SDWebImageRefreshCached progress:^(NSInteger receivedSize,NSInteger expectedSize){
+//            [weakSelf.contentView.posterImage updateImageDownloadProgress:(CGFloat)receivedSize/expectedSize];
+//        }completed:^(UIImage *image,NSError *error,SDImageCacheType cacheType,NSURL *imageURL){
+//            [weakSelf.contentView.posterImage reveal];
+//            weakSelf.backgroundImage.image = image;
+//            weakSelf.contentView.posterImage.userInteractionEnabled = YES;
+//        }];
+//    }else{
+//        [self getMovieIDAndSendRequest];
+//    }
+//}
+//
+//- (void)disappearAndOpenSafari{
+//    [self disappear];
+//    [self performSelector:@selector(openSafari) withObject:self afterDelay:1.5];
+//}
+//
+////显示连接失败提示
+//- (void)showNoticeOfFailure{
+//    _contentView.posterImage.image = nil;
+//    _backgroundImage.image = [UIImage imageNamed:@"p1910907404.jpg"];
+//    MBProgressHUD *hub = [[MBProgressHUD alloc] initWithView:self.view];
+//    [self.view addSubview:hub];
+//    hub.labelText = @"网络连接发生错误";
+//    hub.mode = MBProgressHUDModeText;
+//    [hub showAnimated:YES whileExecutingBlock:^{
+//        sleep(2);
+//    }completionBlock:^{
+//        [hub removeFromSuperViewOnHide];
+//    }];
+//}
+//
+//- (void)openSafari{
+//    NSString *latterPath = [_store getObjectById:@"movie" fromTable:_tableName][@"title"];
+//    NSString *formerPath = @"http://www.soku.com/m/y/video?q=";
+//    NSString *path = [NSString stringWithFormat:@"%@%@",formerPath,latterPath];
+//    NSLog(@"%@",path);
+//    NSString *utf8Path = [path stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+//    NSURL *safariURL = [NSURL URLWithString:utf8Path];
+//    [[UIApplication sharedApplication] openURL:safariURL];
+//}
+//
+//- (void)disappear{
+//    [_nameLabel fadeOut];
+//    [_ratingLabel fadeOut];
+//    [_typeLabel fadeOut];
+//    [self performSelector:@selector(disappearPicture) withObject:self afterDelay:1.5];
+//}
+//
+//- (void)disappearPicture{
+//    _contentView.posterImage.userInteractionEnabled = NO;
+//}
+//
+//#pragma shake
+//-(BOOL)canBecomeFirstResponder
+//{
+//    return YES;
+//}
+//- (void) motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event
+//{
+//    [self disappear];
+//    [self performSelector:@selector(getMovieIDAndSendRequest) withObject:self afterDelay:1.5];
+//}
 
-- (void)disappearAndOpenSafari{
-    [self disappear];
-    [self performSelector:@selector(openSafari) withObject:self afterDelay:1.5];
-}
 
-//显示连接失败提示
-- (void)showNoticeOfFailure{
-    _contentView.posterImage.image = nil;
-    _backgroundImage.image = [UIImage imageNamed:@"p1910907404.jpg"];
-    MBProgressHUD *hub = [[MBProgressHUD alloc] initWithView:self.view];
-    [self.view addSubview:hub];
-    hub.labelText = @"网络连接发生错误";
-    hub.mode = MBProgressHUDModeText;
-    [hub showAnimated:YES whileExecutingBlock:^{
-        sleep(2);
-    }completionBlock:^{
-        [hub removeFromSuperViewOnHide];
-    }];
-}
 
-- (void)openSafari{
-    NSString *latterPath = [_store getObjectById:@"movie" fromTable:_tableName][@"title"];
-    NSString *formerPath = @"http://www.soku.com/m/y/video?q=";
-    NSString *path = [NSString stringWithFormat:@"%@%@",formerPath,latterPath];
-    NSLog(@"%@",path);
-    NSString *utf8Path = [path stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    NSURL *safariURL = [NSURL URLWithString:utf8Path];
-    [[UIApplication sharedApplication] openURL:safariURL];
-}
-
-- (void)disappear{
-    [_nameLabel fadeOut];
-    [_ratingLabel fadeOut];
-    [_typeLabel fadeOut];
-    [self performSelector:@selector(disappearPicture) withObject:self afterDelay:1.5];
-}
-
-- (void)disappearPicture{
-    _contentView.posterImage.userInteractionEnabled = NO;
-}
-
-#pragma shake
--(BOOL)canBecomeFirstResponder
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
 {
     return YES;
-}
-- (void) motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event
-{
-    [self disappear];
-    [self performSelector:@selector(getMovieIDAndSendRequest) withObject:self afterDelay:1.5];
 }
 
 #pragma UIViewControllerTransitioningDelegate
--- OneMovie/MovieView.m
+++ OneMovie/MovieView.m
@@ -7,15 +7,181 @@
 //
 
 #import "MovieView.h"
+#import <POP.h>
+
+@interface MovieView()
+@property (nonatomic) NSUInteger      initialLocation;
+@end
 
 @implementation MovieView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (id)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setUpLayOut];
+    }
+    return self;
 }
-*/
+
+- (void)setUpLayOut{
+    _contentView = [[ContentView alloc] initWithFrame:CGRectMake(10, 10, 300, 425)];
+    _contentView.userInteractionEnabled = YES;
+    [self addSubview:_contentView];
+    
+    _contentView.posterImage.image = [UIImage imageNamed:@"p1910907404.jpg"];
+    UIPanGestureRecognizer *panGesture   = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(pan1:)];
+    [_contentView.posterImage addGestureRecognizer:panGesture];
+}
+
+-(void)pan1:(UIPanGestureRecognizer *)recognizer{
+    CGPoint location = [recognizer locationInView:self];
+    
+    //    static float y_coordinate;
+    //    if (y_coordinate != location.y) {
+    //        y_coordinate = location.y;
+    //        NSLog(@"%f",y_coordinate);
+    //    }
+    
+    //获取手指在PageView中的初始坐标
+    
+    if (recognizer.state == UIGestureRecognizerStateBegan) {
+        self.initialLocation = location.x;
+        [self bringSubviewToFront:_contentView.posterImage];
+    }
+    
+    //    if (recognizer.state == UIGestureRecognizerStateEnded) {
+    //
+    //
+    //        y_coordinate = 0.0f;
+    //    }
+    
+    
+    CGFloat percent = (M_PI /self.initialLocation);
+    
+    if (location.x<=_initialLocation) {
+        if([self isLocation:location InView:self] || location.x<0){
+            
+            CATransform3D move = CATransform3DMakeTranslation(0, 0, 0.1);
+            CATransform3D back = CATransform3DMakeTranslation(0, 0, 0.1);
+            
+            CATransform3D rotate1 = CATransform3DMakeRotation((location.x-self.initialLocation)*percent, 0, 1, 0);
+            CATransform3D rotate2 = CATransform3DMakeRotation(-M_PI+(location.x-self.initialLocation)*percent, 0, 1, 0);
+            
+            CATransform3D mat1 = CATransform3DConcat(CATransform3DConcat(move, rotate1), back);
+            CATransform3D mat2 = CATransform3DConcat(CATransform3DConcat(move, rotate2), back);
+            
+            _contentView.posterImage.layer.transform = CATransform3DPerspect(mat1, CGPointMake(0, 0), 800);
+            _contentView.backView.layer.transform = CATransform3DPerspect(mat2, CGPointMake(0, 0), 800);
+            
+            if ((location.x-self.initialLocation)*percent < -M_PI_2 ){
+                _contentView.posterImage.hidden = YES;
+                _contentView.backView.hidden = NO;
+            }
+            else {
+                _contentView.posterImage.hidden = NO;
+                _contentView.backView.hidden = YES;
+            }
+            
+            //当松手的时候，自动复原
+            if (recognizer.state == UIGestureRecognizerStateEnded ||
+                recognizer.state == UIGestureRecognizerStateCancelled) {
+                if ((location.x-self.initialLocation)*percent > -M_PI/2) {
+                    POPSpringAnimation *recoverAnimation = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerRotationY];
+                    recoverAnimation.springBounciness = 18.0f; //弹簧反弹力度
+                    recoverAnimation.dynamicsMass = 2.0f;
+                    recoverAnimation.dynamicsTension = 200;
+                    recoverAnimation.toValue = @(0);
+                    [_contentView.posterImage.layer pop_addAnimation:recoverAnimation forKey:@"recoverAnimation"];
+                    
+                } else {
+                    POPSpringAnimation *exAnimation = [POPSpringAnimation animationWithPropertyNamed:kPOPViewSize];
+                    exAnimation.springBounciness = 18.0f;
+                    exAnimation.dynamicsMass = 1.0f;
+                    exAnimation.dynamicsTension = 300;
+                    exAnimation.toValue = [NSValue valueWithCGPoint:CGPointMake(self.frame.size.width*1.1 , self.frame.size.height*1.6 )];
+                    [_contentView.backView.layer pop_addAnimation:exAnimation forKey:@"exAnimation"];
+                    
+                    [UIView animateWithDuration:0.5 animations:^{
+                        _contentView.backView.layer.transform = CATransform3DMakeRotation(0 , 0, 1, 0);
+                        
+                    }];
+                    [self performSelector:@selector(changeScrollViewSize) withObject:nil afterDelay:0.1];
+                    
+                    _contentView.noticeLabel.hidden = NO;
+                }
+            }
+        }
+    }
+    if (location.x>0) {
+        //手指超出边界也自动复原
+        if ( location.y<0 || location.y > self.bounds.size.height || (location.x - self.initialLocation)>(CGRectGetWidth(self.bounds))-(self.initialLocation)||location.x<0) {
+            recognizer.enabled = NO;
+            
+            
+            if ((location.x-self.initialLocation)*percent > -M_PI/2) {
+                POPSpringAnimation *recoverAnimation = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerRotationY];
+                recoverAnimation.springBounciness = 18.0f; //弹簧反弹力度
+                recoverAnimation.dynamicsMass = 2.0f;
+                recoverAnimation.dynamicsTension = 200;
+                recoverAnimation.toValue = @(0);
+                [_contentView.posterImage.layer pop_addAnimation:recoverAnimation forKey:@"recoverAnimation"];
+                
+            } else {
+                POPSpringAnimation *exAnimation = [POPSpringAnimation animationWithPropertyNamed:kPOPViewSize];
+                exAnimation.springBounciness = 18.0f;
+                exAnimation.dynamicsMass = 1.0f;
+                exAnimation.dynamicsTension = 300;
+                exAnimation.toValue = [NSValue valueWithCGPoint:CGPointMake(self.frame.size.width*1.1 , self.frame.size.height*1.6 )];
+                [_contentView.backView pop_addAnimation:exAnimation forKey:@"exAnimation"];
+                
+                [UIView animateWithDuration:0.5 animations:^{
+                    _contentView.backView.layer.transform = CATransform3DMakeRotation(0 , 0, 1, 0);
+                    
+                }];
+                [self performSelector:@selector(changeScrollViewSize) withObject:nil afterDelay:0.1];
+                _contentView.noticeLabel.hidden = NO;
+            }
+            
+        }
+        
+        recognizer.enabled = YES;
+    }
+    
+}
+
+- (void)changeScrollViewSize{
+    _contentView.scrollView.frame = CGRectMake(20, 20, self.frame.size.width*1.1-40, self.frame.size.height+80);
+}
+
+-(BOOL)isLocation:(CGPoint)location InView:(UIView *)view{
+    if ((location.y > 0 && location.y < view.bounds.size.height) &&
+        (location.x > 0 && location.x < view.bounds.size.width)) {
+        return YES;
+    }else{
+        return NO;
+    }
+}
+
+//-(CATransform3D)setTransform3D{
+//    CATransform3D transform = CATransform3DIdentity;
+//    transform.m34 = 2.5/-2000;
+//    return transform;
+//}
+
+CATransform3D CATransform3DMakePerspective(CGPoint center, float disZ)
+{
+    CATransform3D transToCenter = CATransform3DMakeTranslation(-center.x, -center.y, 0);
+    CATransform3D transBack = CATransform3DMakeTranslation(center.x, center.y, 0);
+    CATransform3D scale = CATransform3DIdentity;
+    scale.m34 = -1.0f/disZ;
+    return CATransform3DConcat(CATransform3DConcat(transToCenter, scale), transBack);
+}
+
+CATransform3D CATransform3DPerspect(CATransform3D t, CGPoint center, float disZ)
+{
+    return CATransform3DConcat(t, CATransform3DMakePerspective(center, disZ));
+}
+
+
 
 @end
