6c18da91983ecd2d5d0892f22471612faf5b397f
lchaooo <lichao@lichaodeMacBook-Air.local>
2015-04-08 19:59:21 +0800

    Revert 862ca66..09cbbd5
    This rolls back to commit 862ca666053b9607a5c744b5286b4b2f78214432.

---
+++ OneMovie/BookView.h
@@ -0,0 +1,13 @@
+//
+//  BookView.h
+//  OneMovie
+//
+//  Created by 俞 丽央 on 15-4-4.
+//  Copyright (c) 2015年 李超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BookView : UIView
+
+@end
